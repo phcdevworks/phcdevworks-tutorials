9e2f8086c0cafe121961b92b051330860ff1f77a
BradPotts <brad.potts@phcnet.co>
2019-09-13 12:08:58 +0000

    add frontend framework.

---
+++ app/controllers/phcdevworks_tutorials/blog/tutorials_controller.rb
@@ -0,0 +1,6 @@
+require_dependency "phcdevworks_tutorials/application_controller"
+
+module PhcdevworksTutorials
+  class Blog::TutorialsController < ApplicationController
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,19 @@ PhcdevworksTutorials::Engine.routes.draw do
     resources :categories, class_name: 'PhcdevworksTutorials::Tutorial::Category' 
   end
 
+  # Frontend Routes
+  namespace :blog do
+    resources :tutorials, only: [:index, :show]
+  end
+
+  # API Routes
+  namespace :api, :path => "", :constraints => {:subdomain => "tutorial_api"} do
+    namespace :v1 do
+      resources :posts, defaults: {format: 'json'}
+      resources :categories, defaults: {format: 'json'}
+    end
+  end
+
   # Mount Routes
   mount PhcdevworksAccounts::Engine, :at => '/'
 
