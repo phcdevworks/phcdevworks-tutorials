1da1f025830b50cfdc35040788b0ef945dcc3505
BradPotts <brad.potts@phcnet.co>
2019-10-06 23:31:04 +0000

    add missing migration.

--- db/migrate/20191002093451_add_image_to_phcdevworks_tutorials_tutorial_steps.rb
+++ db/migrate/20191002093451_add_image_to_phcdevworks_tutorials_tutorial_steps.rb
@@ -2,7 +2,6 @@ class AddImageToPhcdevworksTutorialsTutorialSteps < ActiveRecord::Migration[6.0]
   def change
 
     add_column :phcdevworks_tutorials_tutorial_steps, :tutorial_step_image, :string
-    add_column :phcdevworks_tutorials_tutorial_posts, :tutorial_post_image, :string
 
   end
 end
