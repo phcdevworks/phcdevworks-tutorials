2d7c5fe22b7a47865f3fa885fe0551fbfff7c090
BradPotts <brad.potts@phcnet.co>
2020-02-19 10:43:36 +0000

    adjusted add on step db.

--- db/migrate/20191110232654_add_copy_instructions_to_steps.rb
+++ db/migrate/20191110232654_add_copy_instructions_to_steps.rb
@@ -1,7 +1,7 @@
 class AddCopyInstructionsToSteps < ActiveRecord::Migration[6.0]
   def change
   
-    add_column :phcdevworks_tutorials_tutorial_steps, :step_copy_instruction, :string
+    add_column :phcdevworks_tutorials_tutorial_steps, :step_copy_instruction, :text
   
   end
 end
